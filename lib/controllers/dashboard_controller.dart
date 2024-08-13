4dae27ece4c37718785c5f52244f2526a1eb5bbe
Hafizh <Hafizh@gmail.com>
2024-08-13 12:17:06 +0700

    Fix login data, create fragment for dashboard, ctrate dashboard controller, dashboard page

---
+++ lib/controllers/dashboard_controller.dart
@@ -0,0 +1,45 @@
+import 'package:d_view/d_view.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_coworkers_app/pages/fragment/browse_fragment.dart';
+import 'package:flutter_coworkers_app/pages/fragment/order_fragment.dart';
+import 'package:get/get.dart';
+
+class DashboardController extends GetxController {
+  clear() {
+    Get.delete<DashboardController>(force: true);
+  }
+
+  final _index = 0.obs;
+  int get index => _index.value;
+  set index(int n) => _index.value = n;
+
+  // Getter ambil fragment yang aktif
+  Widget get currentFragment => menu[index]['fragment'];
+
+  List<Map> menu = [
+    {
+      'icon_off': 'assets/ic_browse_grey.png',
+      'icon_on': 'assets/ic_browse_purple.png',
+      'label': 'Browse',
+      'fragment': BrowseFragment(),
+    },
+    {
+      'icon_off': 'assets/ic_orders_grey.png',
+      'icon_on': 'assets/ic_orders_purple.png',
+      'label': 'Order',
+      'fragment': OrderFragment(),
+    },
+    {
+      'icon_off': 'assets/ic_wallet_grey.png',
+      'icon_on': 'assets/ic_wallet_grey.png',
+      'label': 'Wallet',
+      'fragment': DView.empty('Wallet'),
+    },
+    {
+      'icon_off': 'assets/ic_settings_grey.png',
+      'icon_on': 'assets/ic_settings_purple.png',
+      'label': 'Settings',
+      'fragment': OrderFragment(),
+    },
+  ];
+}
--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -1,13 +1,62 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:d_view/d_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_coworkers_app/controllers/dashboard_controller.dart';
+import 'package:get/get.dart';
 
-class Dashboard extends StatelessWidget {
+class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  // Buat object
+  final dashboardController = Get.put(DashboardController());
+
+  @override
+  void dispose() {
+    dashboardController.clear();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: DView.appBarCenter('Dashboard'),
+      body: Obx(() => dashboardController.currentFragment),
+      bottomNavigationBar: Obx(
+        () {
+          // Wrap dengan Obx agar icon bisa berpindah
+          return BottomNavigationBar(
+            backgroundColor: Colors.white,
+            currentIndex: dashboardController.index,
+            type: BottomNavigationBarType.fixed,
+            onTap: (value) {
+              dashboardController.index = value;
+            },
+            selectedFontSize: 14,
+            unselectedFontSize: 14,
+            selectedItemColor: Theme.of(context).primaryColor,
+            unselectedItemColor: Colors.grey,
+            selectedLabelStyle: TextStyle(
+              fontWeight: FontWeight.bold,
+              height: 2,
+            ),
+            unselectedLabelStyle: TextStyle(
+              fontWeight: FontWeight.bold,
+              height: 2,
+            ),
+            items: dashboardController.menu.map((e) {
+              return BottomNavigationBarItem(
+                  icon: ImageIcon(AssetImage(e['icon_off'])),
+                  activeIcon: ImageIcon(AssetImage(e['icon_on'])),
+                  label: e['label']);
+            }).toList(),
+          );
+        },
+      ),
     );
   }
 }
